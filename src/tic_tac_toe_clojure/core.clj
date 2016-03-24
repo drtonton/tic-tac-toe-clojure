29113502bd2a12fd0ae76a6a75f77025266217a2
drtonton <tkfoster26@gmail.com>
2016-03-24 10:31:27 -0400

    algorithm works

--- src/tic_tac_toe_clojure/core.clj
+++ src/tic_tac_toe_clojure/core.clj
@@ -1,5 +1,15 @@
 (ns tic-tac-toe-clojure.core
   (:gen-class))
 
+(def board [[:e :x :e]
+            [:o :o :o]
+            [:x :e :x]])
+
+(defn solve [x]
+  (let [[[a b c]
+         [d e f]
+         [g h i]] x]
+    (println a b c d e f g h i)))
+
 (defn -main []
   (println "Hello, World!"))
